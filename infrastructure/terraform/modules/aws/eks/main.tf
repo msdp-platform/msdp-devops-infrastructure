9f66bd5087fbce6cc97b8eeb486734663241f33f
Santanu Biswas <santanu@MacBookPro.broadband>
2025-09-12 05:00:38 +0100

    fix: refactor HCL blocks to multi-line format and add fmt validation
    - Convert single-line variable blocks to multi-line format in AWS EKS module
    - Convert single-line output blocks to multi-line format in AWS EKS module
    - Add terraform fmt -recursive -check step to workflow before init
    - All HCL blocks now follow proper multi-line format
    - Terraform validation passes for both AWS and Azure stacks

--- infrastructure/terraform/modules/aws/eks/main.tf
+++ infrastructure/terraform/modules/aws/eks/main.tf
@@ -39,7 +39,15 @@ resource "aws_eks_node_group" "default" {
   })
 }
 
-output "cluster_name" { value = aws_eks_cluster.this.name }
-output "cluster_endpoint" { value = aws_eks_cluster.this.endpoint }
-output "cluster_certificate_authority_data" { value = aws_eks_cluster.this.certificate_authority[0].data }
+output "cluster_name" {
+  value = aws_eks_cluster.this.name
+}
+
+output "cluster_endpoint" {
+  value = aws_eks_cluster.this.endpoint
+}
+
+output "cluster_certificate_authority_data" {
+  value = aws_eks_cluster.this.certificate_authority[0].data
+}
 
--- infrastructure/terraform/modules/aws/eks/variables.tf
+++ infrastructure/terraform/modules/aws/eks/variables.tf
@@ -1,19 +1,45 @@
-variable "org" { type = string }
-variable "env" { type = string }
-variable "cloud" { type = string }
-variable "region" { type = string }
-variable "cluster_name" { type = string }
-variable "vpc_id" { type = string }
-variable "private_subnet_ids" { type = list(string) }
-variable "k8s_version" { type = string }
+variable "org" {
+  type = string
+}
+
+variable "env" {
+  type = string
+}
+
+variable "cloud" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "private_subnet_ids" {
+  type = list(string)
+}
+
+variable "k8s_version" {
+  type = string
+}
 variable "node_group" {
   type = object({
-    name         = string
-    instance_type= string
-    desired_size = number
-    min_size     = number
-    max_size     = number
+    name          = string
+    instance_type = string
+    desired_size  = number
+    min_size      = number
+    max_size      = number
   })
 }
-variable "tags" { type = map(string) default = {} }
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
 
