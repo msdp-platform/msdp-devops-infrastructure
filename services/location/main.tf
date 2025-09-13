f79a83ebaab1e9dbf0b9ac7412c8e6eb34346a63
santanu <santanubiswas2k@gmail.com>
2025-09-13 12:58:16 +0100

    ok

--- services/location/main.tf
+++ services/location/main.tf
@@ -15,5 +15,71 @@ provider "aws" {
 resource "aws_apigatewayv2_api" "http" {
   name          = "${var.project}-location-${var.env}"
   protocol_type = "HTTP"
+
+  cors_configuration {
+    allow_credentials = false
+    allow_headers     = ["*"]
+    allow_methods     = ["GET", "POST", "OPTIONS"]
+    allow_origins     = var.cors_allowed_origins
+  }
 }
 
+resource "aws_iam_role" "lambda_exec" {
+  name               = "${var.project}-location-exec-${var.env}"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume.json
+}
+
+data "aws_iam_policy_document" "lambda_assume" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "location" {
+  function_name = "${var.project}-location-${var.env}"
+  package_type  = var.lambda_image_uri != "" ? "Image" : "Zip"
+  image_uri     = var.lambda_image_uri
+  role          = aws_iam_role.lambda_exec.arn
+}
+
+resource "aws_apigatewayv2_integration" "lambda" {
+  api_id                 = aws_apigatewayv2_api.http.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.location.invoke_arn
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "city" {
+  api_id    = aws_apigatewayv2_api.http.id
+  route_key = "GET /locations/{country}/{city}"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
+}
+
+resource "aws_apigatewayv2_route" "category" {
+  api_id    = aws_apigatewayv2_api.http.id
+  route_key = "GET /locations/{country}/{city}/{category}"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
+}
+
+resource "aws_apigatewayv2_stage" "dev" {
+  api_id      = aws_apigatewayv2_api.http.id
+  name        = var.env
+  auto_deploy = true
+}
+
+resource "aws_lambda_permission" "allow_apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.location.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.http.execution_arn}/*/*"
+}
