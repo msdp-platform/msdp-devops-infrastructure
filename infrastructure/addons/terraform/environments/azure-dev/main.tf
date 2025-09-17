b7aa6c4e8ec1447e2e11941aa0361dfbd898d8d9
santanu <santanubiswas2k@gmail.com>
2025-09-17 11:17:49 +0100

    fic cert-manager-issuer --namespace cert-manager --cluster-issuer letsencrypt-prod

--- infrastructure/addons/terraform/environments/azure-dev/main.tf
+++ infrastructure/addons/terraform/environments/azure-dev/main.tf
@@ -37,18 +37,16 @@ provider "azurerm" {
 }
 
 provider "kubernetes" {
-  host                   = data.azurerm_kubernetes_cluster.cluster.kube_config.0.host
-  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.client_certificate)
-  client_key             = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.client_key)
-  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate)
+  # Use the kubectl context configured by the workflow (az aks get-credentials --admin)
+  load_config_file = true
+  config_path      = "~/.kube/config"
 }
 
 provider "helm" {
   kubernetes {
-    host                   = data.azurerm_kubernetes_cluster.cluster.kube_config.0.host
-    client_certificate     = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.client_certificate)
-    client_key             = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.client_key)
-    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate)
+    # Use the same kubeconfig as the Kubernetes provider
+    load_config_file = true
+    config_path      = "~/.kube/config"
   }
 }
 
